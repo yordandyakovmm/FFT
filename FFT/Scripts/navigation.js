0b44e802faac2c584171f157803a4c87ded18c96
Yordan Dyakov <ydyakov@questarai.com>
2019-01-03 19:24:16 +0200

    all

--- FFT/Scripts/navigation.js
+++ FFT/Scripts/navigation.js
@@ -1,35 +1,18 @@
 ﻿$('body').keyup(function (e) {
-    debugger;
+
+
     if (!e.altKey) {
         return;
     }
 
     // -->
     if (e.which == 39) {
-        $('body');
-            window.location = frward;
+       window.location = frward;
     }
     // <--
     if (e.which == 37) {
-        $('body');
-            window.location = backward;
+        window.location = backward;
     }
-
-    if (e.which == 81 || e.which == 113) {
-        $('body');
-            window.location ='/';
-    }
-
-    if (e.which == 87 || e.which == 119) {
-        $('body');
-            window.location = '/dft';
-    }
-
-    if (e.which == 69 || e.which == 101) {
-        $('body');
-         window.location = '/dft1';
        
-    }
-
 
 });
